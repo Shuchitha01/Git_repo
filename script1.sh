c65678ebbd14dc1008c2fd2e778bd95aae82c8da
Ubuntu <ubuntu@ip-172-31-5-74.ap-south-1.compute.internal>
2025-11-05 14:00:33 +0000

    free-h

--- script1.sh
+++ script1.sh
@@ -2,7 +2,7 @@
 
 size=$(free -h | head -2 | tail -1 | awk '{print $6}' | sed 's/Mi//g')
 
-if [ "$size" -gt 100 ]; then
+if [ "$size" -gt 50 ]; then
     echo "Current usage is ${size}MB" > percent2
     # mail -s "RAM usage alert" -c "abc@gmail.com" < percent2
 fi
